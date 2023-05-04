dada8b6c8f6e1ba492e440b80a27a05955742456
Georgii Karateev <Georgy.karat@gmail.com>
2023-05-04 22:55:07 +0300

    Home blank page added

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http.response import JsonResponse
+
 
 # Create your views here.
+
+def home(request, *args, **kwargs):
+    return render(request, 'home/home.html', {
+    })
+
+
