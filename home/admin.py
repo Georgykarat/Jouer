c69d11f355b0b9add23c865a401ac3f3b5c57cde
Georgii Karateev <Georgy.karat@gmail.com>
2023-05-07 13:48:42 +0300

    Model signup

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from home.models import SignUpModel
 
 # Register your models here.
+
+class SignUpAdmin(admin.ModelAdmin):
+    list_display = ['mail','code','timestamp']
+    search_fields = ['mail','code']
+
+
+
+
+admin.site.register(SignUpModel,SignUpAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class SignUpModel(models.Models):
+    mail = models.EmailField()
+    code = models.CharField(max_length=6)
+    timestamp = models.DateTimeField(auto_now_add=True)
