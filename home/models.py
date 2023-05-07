8e0e3f2619a29a281bb9950a4f451dad325b3a65
Georgii Karateev <Georgy.karat@gmail.com>
2023-05-07 13:56:03 +0300

    1st migration

--- home/models.py
+++ home/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class SignUpModel(models.Models):
+class SignUpModel(models.Model):
     mail = models.EmailField()
     code = models.CharField(max_length=6)
     timestamp = models.DateTimeField(auto_now_add=True)
