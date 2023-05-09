84f723e7b2b102252af26e26a99334022ec518a2
Georgii Karateev <Georgy.karat@gmail.com>
2023-05-09 15:00:57 +0300

    If auth in feed

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 
 def feed(request):
-    return render(request, 'feed/feed.html')
+    if request.user.is_authenticated == True:
+        return render(request, 'feed/feed.html')
+    else:
+        return HttpResponseRedirect('/login')
