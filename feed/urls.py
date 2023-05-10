b2a9a1ecd24ef77675ad6e4aa73a05042092f40f
Georgii Karateev <Georgy.karat@gmail.com>
2023-05-10 17:25:42 +0300

    Logout

--- feed/urls.py
+++ feed/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from feed.views import MainLogoutView
 from .import views
 
 urlpatterns = [
     path("", views.feed, name="feed"),
+    path("logout/", MainLogoutView.as_view(), name="logout"),
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponseRedirect
+from django.contrib.auth.views import LogoutView
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,4 +8,14 @@ def feed(request):
     if request.user.is_authenticated == True:
         return render(request, 'feed/feed.html')
     else:
-        return HttpResponseRedirect('/login')
+        return HttpResponseRedirect('/login/')
+    
+
+# def logout(request):
+#     """Provides users the ability to logout (func)"""
+#     return HttpResponseRedirect('/')
+
+
+class MainLogoutView(LogoutView):
+    """Provides users the ability to logout (class)"""
+    next_page = '/'
