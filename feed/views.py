eaf36283668eb018009724aea9eb5ee5f7e0bd2a
Georgii Karateev <Georgy.karat@gmail.com>
2023-05-16 23:07:33 +0300

    cropper upd2

--- feed/views.py
+++ feed/views.py
@@ -6,6 +6,8 @@ from home.models import CustomUser
 from django.conf import settings
 from home.views import is_ajax
 from django.contrib.auth import authenticate, update_session_auth_hash
+import os
+from PIL import Image
 
 
 class Design:
@@ -94,6 +96,61 @@ def upload_photo(request):
         # Process the uploaded photo here
         # Save the cropped photo as the profile picture
         if request.user.is_authenticated == True:
+            #______________
+            photo = request.FILES['photo']
+
+            # Create the 'temp' directory if it doesn't exist
+            if not os.path.exists('temp'):
+                os.makedirs('temp')
+
+            # Save the uploaded photo temporarily
+            photo_path = 'temp/' + photo.name
+            with open(photo_path, 'wb') as f:
+                for chunk in photo.chunks():
+                    f.write(chunk)
+
+            # Get the cropping coordinates
+            crop_x = request.POST.get('crop_x')
+            crop_y = request.POST.get('crop_y')
+            crop_width = request.POST.get('crop_width')
+            crop_height = request.POST.get('crop_height')
+
+            print('crop_x:', crop_x)
+            print('crop_y:', crop_y)
+            print('crop_width:', crop_width)
+            print('crop_height:', crop_height)
+
+            # Perform error checking for the cropping coordinates
+            if crop_x is None or crop_y is None or crop_width is None or crop_height is None:
+                return render(request, 'upload.html', {'error': 'Invalid cropping coordinates'})
+
+            try:
+                crop_x = float(crop_x)
+                crop_y = float(crop_y)
+                crop_width = float(crop_width)
+                crop_height = float(crop_height)
+            except ValueError:
+                return render(request, 'upload.html', {'error': 'Invalid cropping coordinates'})
+
+            # Open the uploaded photo using Pillow
+            image = Image.open(photo_path)
+
+            # Calculate the cropping region
+            left = int(crop_x)
+            top = int(crop_y)
+            right = int(crop_x + crop_width)
+            bottom = int(crop_y + crop_height)
+
+            # Crop the image
+            cropped_image = image.crop((left, top, right, bottom))
+
+            # Save the cropped image as the profile picture
+            cropped_photo_path = 'temp/cropped_' + photo.name
+            cropped_image.save(cropped_photo_path)
+
+            # Remove the temporary uploaded photo
+            os.remove(photo_path)
+            #______________
             CurrentUser = Person(request)
             CurrentDesign = Design()
             return render(request, 'settings/settings.html', {
