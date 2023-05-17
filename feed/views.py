5e359cd0711ed1ad08d35a8deeede7423255236a
Georgii Karateev <Georgy.karat@gmail.com>
2023-05-17 22:37:10 +0300

    alloed extensions and max size added

--- feed/views.py
+++ feed/views.py
@@ -19,6 +19,7 @@ class Design:
 class Person:
     """Class for getting user data"""
     def __init__(self, request):
+        self.userid = request.user.id
         self.mail = request.user.username
         self.userdata = CustomUser.objects.filter(user=request.user).values().get()
         self.firstname = self.userdata['first_name']
@@ -96,68 +97,74 @@ def upload_photo(request):
         # Process the uploaded photo here
         # Save the cropped photo as the profile picture
         if request.user.is_authenticated == True:
-            #______________
+            CurrentUser = Person(request)
             photo = request.FILES['photo']
 
             # Create the 'temp' directory if it doesn't exist
             if not os.path.exists('temp'):
                 os.makedirs('temp')
+            # Get extension, to assign proper name later
+            allowed_extensions = ['jpg', 'jpeg', 'png', 'gif']
+            file_extension = photo.name.split('.')[-1].lower()
+            if file_extension in allowed_extensions:
+                # Check file size
+                if photo.size <= 20 * 1024 * 1024:
+                    # Save the uploaded photo temporarily
+                    photo_path = 'temp/' + str(CurrentUser.userid) + '.' + file_extension
+                    with open(photo_path, 'wb') as f:
+                        for chunk in photo.chunks():
+                            f.write(chunk)
 
-            # Save the uploaded photo temporarily
-            photo_path = 'temp/' + photo.name
-            with open(photo_path, 'wb') as f:
-                for chunk in photo.chunks():
-                    f.write(chunk)
+                    # Get the cropping coordinates
+                    crop_x = request.POST.get('crop_x')
+                    crop_y = request.POST.get('crop_y')
+                    crop_width = request.POST.get('crop_width')
+                    crop_height = request.POST.get('crop_height')
 
-            # Get the cropping coordinates
-            crop_x = request.POST.get('crop_x')
-            crop_y = request.POST.get('crop_y')
-            crop_width = request.POST.get('crop_width')
-            crop_height = request.POST.get('crop_height')
+                    print('crop_x:', crop_x)
+                    print('crop_y:', crop_y)
+                    print('crop_width:', crop_width)
+                    print('crop_height:', crop_height)
 
-            print('crop_x:', crop_x)
-            print('crop_y:', crop_y)
-            print('crop_width:', crop_width)
-            print('crop_height:', crop_height)
+                    # Perform error checking for the cropping coordinates
+                    if crop_x is None or crop_y is None or crop_width is None or crop_height is None:
+                        return render(request, 'upload.html', {'error': 'Invalid cropping coordinates'})
+                    try:
+                        crop_x = float(crop_x)
+                        crop_y = float(crop_y)
+                        crop_width = float(crop_width)
+                        crop_height = float(crop_height)
+                    except ValueError:
+                        return render(request, 'upload.html', {'error': 'Invalid cropping coordinates'})
 
-            # Perform error checking for the cropping coordinates
-            if crop_x is None or crop_y is None or crop_width is None or crop_height is None:
-                return render(request, 'upload.html', {'error': 'Invalid cropping coordinates'})
+                    # Open the uploaded photo using Pillow
+                    image = Image.open(photo_path)
 
-            try:
-                crop_x = float(crop_x)
-                crop_y = float(crop_y)
-                crop_width = float(crop_width)
-                crop_height = float(crop_height)
-            except ValueError:
-                return render(request, 'upload.html', {'error': 'Invalid cropping coordinates'})
+                    # Calculate the cropping region
+                    left = int(crop_x)
+                    top = int(crop_y)
+                    right = int(crop_x + crop_width)
+                    bottom = int(crop_y + crop_height)
 
-            # Open the uploaded photo using Pillow
-            image = Image.open(photo_path)
+                    # Crop the image
+                    cropped_image = image.crop((left, top, right, bottom))
 
-            # Calculate the cropping region
-            left = int(crop_x)
-            top = int(crop_y)
-            right = int(crop_x + crop_width)
-            bottom = int(crop_y + crop_height)
+                    # Save the cropped image as the profile picture
+                    cropped_photo_path = 'rootmedia/profile_images/i' + str(CurrentUser.userid) + '.' + file_extension
+                    cropped_image.save(cropped_photo_path)
 
-            # Crop the image
-            cropped_image = image.crop((left, top, right, bottom))
-
-            # Save the cropped image as the profile picture
-            cropped_photo_path = 'temp/cropped_' + photo.name
-            cropped_image.save(cropped_photo_path)
-
-            # Remove the temporary uploaded photo
-            os.remove(photo_path)
-            #______________
-            CurrentUser = Person(request)
-            CurrentDesign = Design()
-            return render(request, 'settings/settings.html', {
-                'page': 'settings',
-                'cuser': CurrentUser,
-                'design': CurrentDesign,
-            })
+                    # Remove the temporary uploaded photo
+                    os.remove(photo_path)
+                    CurrentDesign = Design()
+                    return render(request, 'settings/settings.html', {
+                        'page': 'settings',
+                        'cuser': CurrentUser,
+                        'design': CurrentDesign,
+                    })
+                else:
+                    return JsonResponse({}, status=400)
+            else:
+                return JsonResponse({}, status=400)
         else:
             return JsonResponse({}, status=400)
     return JsonResponse({}, status=400)
